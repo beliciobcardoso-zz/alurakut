5d28502a0142e8c3c8d28ef93fa143486497f8a0
Belicio Batista Cardoso <belloinfo@gmail.com>
2021-07-13 01:51:03 -0300

    Concluindo Aula 01

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,34 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+:root {
+  --backgroundPrimary: #D9E6F6;
+}
+* {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
 }
+  body {
+    font-family: sans-serif;
+    background-color: var(--backgroundPrimary);
+  }
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+  ${AlurakutStyles}
+`
+const theme = {
 
+}
 export default function App({ Component, pageProps }) {
   return (
     <>
